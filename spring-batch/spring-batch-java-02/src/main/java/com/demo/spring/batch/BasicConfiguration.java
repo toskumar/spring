696cb9d8bc659002f2f41c9ca4808ca5c4bf33f3
SenthilKumar <toskumar@yahoo.com>
2020-05-07 15:37:40 +0530

    organized import

--- spring-batch/spring-batch-java-02/src/main/java/com/demo/spring/batch/BasicConfiguration.java
+++ spring-batch/spring-batch-java-02/src/main/java/com/demo/spring/batch/BasicConfiguration.java
@@ -4,24 +4,10 @@ import java.net.MalformedURLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -44,7 +30,7 @@ public class BasicConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.sqlite.JDBC");
-		dataSource.setUrl("jdbc:sqlite:spring_batch02a_sqlite.db");
+		dataSource.setUrl("jdbc:sqlite:batch_sqlite1.db");
 
 		return dataSource;
 	}
@@ -53,7 +39,7 @@ public class BasicConfiguration {
 	public DataSource dataSource1() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.sqlite.JDBC");
-		dataSource.setUrl("jdbc:sqlite:spring_batch02b_sqlite.db");
+		dataSource.setUrl("jdbc:sqlite:batch_sqlite2.db");
 
 		return dataSource;
 	}
